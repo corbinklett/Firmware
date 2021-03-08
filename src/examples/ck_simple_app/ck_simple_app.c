c906229854a73dd87bdb4151497c6c19afcda1b3
Corbin Klett <corbinklett@gmail.com>
2021-03-08 08:59:48 -0500

    started hover ctrl

--- src/examples/ck_simple_app/ck_simple_app.c
+++ src/examples/ck_simple_app/ck_simple_app.c
@@ -23,33 +23,65 @@ int ck_simple_app_main(int argc, char *argv[])
 	PX4_INFO("Hello sky");
 
 	/* subscribe to sensor_combined topic */
-	int sensor_sub_fd =
+	/* fd = file descriptor?? */
+	int sensor_sub_fd = orb_subscribe(ORB_ID(sensor_combined));
+	/* limit the update rate to 5 Hz */
+	orb_set_interval(sensor_sub_fd, 200);
 
+	/* advertise attitude topic */
+	struct vehicle_attitude_s att;
+	memset(&att, 0, sizeof(att));
+	orb_advert_t att_pub = orb_advertise(ORB_ID(vehicle_attitude), &att);
 
-	return OK;
-}
+	/* one could wait for multiple topics with this technique, just using one here */
+	px4_pollfd_struct_t fds[] = {
+		{ .fd = sensor_sub_fd, .events = POLLIN}
+		/* can also use .fd = other things... */
+	};
 
-int sensor_sub_fd = orb_subscribe(ORB_ID(sensor_combined));
+	int error_counter = 0;
 
-/* one could wait for multiple topics with this technique, just using one here */
-px4_pollfd_struct_t fds[] = {
-	{ .fd = sensor_sub_fd,	.events = POLLIN }
-}
+	for (int i = 0; i < 5; i++) {
+		/* wait for sensor update of 1 file descriptor for 1000 ms (1 second) */
+		int poll_ret = px4_poll(fds, 1, 1000);
 
-while (true) {
-	// wait for sensor update of 1 file descriptor for 1000 ms
-	int poll_ret = px4_poll(fds, 1, 1000);
+		/* handle the poll result */
+		if (poll_ret == 0) {
+			/* this means none of our providers is giving us data */
+			PX4_ERR("Got no data within a second");
+		} else if (poll_ret < 0) {
+			/* this is seriously bad - should be an emergency */
+			if (error_counter < 10 || error_counter % 50 == 0) {
+			/* use a counter to prevent flooding (and slowing us down) */
+			PX4_ERR("ERROR return value from poll(): %d", poll_ret);
+			}
 
-	if (fds[0].revents & POLLIN) {
-		// obtained data for the first file descriptor
-		struct sensor_combined_s raw;
-		// copy sensors raw data into local buffer
-		orb_copy(ORB_ID(sensor_combined), sensor_sub_fd, &raw);
-		PX4_INFO("Accelerometer:\t%8.4f\t%8.4f\t%8.4f",
-			(double)raw.accelerometer_m_s2[0],
-			(double)raw.accelerometer_m_s2[1],
-			(double)raw.accelerometer_m_s2[2]);
+			error_counter++;
+		} else {
+			if (fds[0].revents & POLLIN) {
+				/* obtained data for the first file descriptor */
+				struct sensor_combined_s raw;
+				/* copy sensors raw data into local buffer */
+				orb_copy(ORB_ID(sensor_combined), sensor_sub_fd, &raw);
+				PX4_INFO("Accelerometre: \t%8.4f\t%8.4f\t%8.4f",
+					(double)raw.accelerometer_m_s2[0],
+					(double)raw.accelerometer_m_s2[1],
+					(double)raw.accelerometer_m_s2[2]);
+				/* set att and publish this information for other apps
+				the following does not have any meaning, it's just an example */
+
+				att.q[0] = raw.accelerometer_m_s2[0];
+				att.q[1] = raw.accelerometer_m_s2[1];
+				att.q[2] = raw.accelerometer_m_s2[2];
+
+				orb_publish(ORB_ID(vehicle_attitude), att_pub, &att);
+
+				/* there could be more file descriptors here, in the form like:
+				* if (fds[1..n].revents & POLLIN) {}
+				*/
+			}
+		}
 	}
+	PX4_INFO("exiting");
+	return 0;
 }
-
-
